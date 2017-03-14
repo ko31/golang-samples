b6bdb26b9a6c8b7452db48a1595d5c250888f1cc
Ko Takagi <knockoutmarch@gmail.com>
2017-03-14 16:31:04 +0900

    add:vimeo-thumbnail

---
+++ vimeo-thumbnail/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Vimeo struct {
+	Thumbnail_url string
+}
+
+//var id = "76979871"
+
+func run() error {
+
+	var url = "https://vimeo.com/api/oembed.json?url=https://vimeo.com/"
+
+	if len(os.Args) < 2 {
+		return fmt.Errorf("Parameter is invalid")
+	}
+
+	url += os.Args[1]
+
+	if len(os.Args) == 3 {
+		url += "&width=" + os.Args[2]
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("Failed to connect api")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Failed to read response")
+	}
+
+	jsonBytes := ([]byte)(body)
+	data := new(Vimeo)
+
+	if err := json.Unmarshal(jsonBytes, data); err != nil {
+		return fmt.Errorf("Json Unmarshal error")
+	}
+
+	fmt.Printf("%s", data.Thumbnail_url)
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
